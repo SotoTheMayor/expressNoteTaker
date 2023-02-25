1e48837fc38e3947540df963ba59e35cc9cb97a9
Soto <quickfire25@yahoo.com>
2023-02-25 15:29:22 -0600

    writing to db file

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path')
 const fs = require('fs');
-const db = require('./Develop/db/db.json')
+const db = require('./Develop/db/db.json');
+const { stringify } = require('querystring');
 
 const PORT = 3001;
 const app = express();
 
-app.use(express.static(__dirname + '/Develop/public'))
+app.use(express.static(__dirname + '/Develop/public'));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
@@ -17,7 +19,31 @@ app.get('/notes', (req, res) => {
 })
 
 app.get('/api/notes', (req, res) => res.json(db))
-    
+
+app.post('/api/notes', (req, res) => {
+    console.log(req.body);
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text
+    }
+
+    const noteString = JSON.stringify(newNote);
+
+    fs.writeFile(`./Develop/db/db.json`, noteString, (err) =>
+    err ? console.error(err) : console.log(`Review for ${newNote.title} has been written to file`)
+    )
+
+    const response = {
+        status: "Success",
+        body: newNote
+    }
+
+    res.status(201).json(response)
+})
+
+
 app.listen(PORT, () => 
     console.log(`Listening at http://localhost:${PORT}`)
 )
