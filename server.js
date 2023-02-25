5007eb27ac86bbf1285bce794fc9737875e9b9a5
Soto <quickfire25@yahoo.com>
2023-02-25 16:06:02 -0600

    fsutils and uuid added

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const path = require('path')
 const fs = require('fs');
 const db = require('./Develop/db/db.json');
-const { stringify } = require('querystring');
+const { readFromFile, readAndAppend } = require('./helpers/fsUtils');
 
 const PORT = 3001;
 const app = express();
 
 app.use(express.static(__dirname + '/Develop/public'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'Develop/public/index.html'))
@@ -29,11 +30,11 @@ app.post('/api/notes', (req, res) => {
         text
     }
 
-    const noteString = JSON.stringify(newNote);
+    readAndAppend(newNote, `./Develop/db/db.json`)
 
-    fs.writeFile(`./Develop/db/db.json`, noteString, (err) =>
-    err ? console.error(err) : console.log(`Review for ${newNote.title} has been written to file`)
-    )
+    // fs.writeFile(`./Develop/db/db.json`, JSON.stringify(newNote, null, `\t`), (err) =>
+    // err ? console.error(err) : console.log(`Review for ${newNote.title} has been written to file`)
+    // )
 
     const response = {
         status: "Success",
